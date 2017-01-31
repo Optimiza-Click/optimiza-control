819577cffae20b20567a9dc6a65c7729bd98aa47
Luis David <luisdavid.del.barrio@gmail.com>
2017-01-31 09:32:39 +0100

    add to remove problems with woocommerce

--- includes/sessions.php
+++ includes/sessions.php
@@ -6,14 +6,11 @@ if ( ! class_exists( 'Remove_Sessions' ) ) {
             register_activation_hook(__FILE__, array( $this,'wp_cron'));
             add_action('data_cron', array( $this,'remove'));
         }
-
-// function to start cron job to delete wc_sessions, only if woocommerce exists :)        
+        
         public function wp_cron() {
-			if ( class_exists( 'WooCommerce' ) ) {
-				if (! wp_next_scheduled ( 'data_cron' )) {
-					wp_schedule_event(time(), 'daily', 'data_cron');
+			 if (! wp_next_scheduled ( 'data_cron' )) {
+				wp_schedule_event(time(), 'daily', 'data_cron');
 				 }
-			}
         }
                  
         public function remove() {
